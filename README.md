4eba96ae776a2ff2db6095a10f9d0552937b9c4a
Janey_raspberryPi <1737941060@qq.com>
2021-04-24 09:06:53 +0800

    [ESP] update Delay_us

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 宿舍远程开空调 项目简称SB611啦   ^=^
 ## 进度与贡献者
 - [x] ESP32联网 -Janey7695
-- [x] Mqtt服务器架设 -Janer7695
-- [ ] 红外发射模块编制
+- [x] Mqtt服务器架设 -Janey7695
+- [x] 红外发射模块编制 - WSH-FJ
+- [x] us level delay -X733
+- [ ] IRAD circuit
--- main/app_main.c
+++ main/app_main.c
@@ -1,12 +1,3 @@
-/* MQTT (over TCP) Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -17,9 +8,7 @@
 #include "esp_event.h"
 #include "tcpip_adapter.h"
 #include "protocol_examples_common.h"
-#include "driver/periph_ctrl.h"
-#include "driver/timer.h"
-#include "esp_types.h"
+#include "esp32/rom/ets_sys.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -38,42 +27,18 @@
 
 #define LED 2
 #define IRDA_Pin 15
-static const char *TAG = "MQTT_EXAMPLE";
+static const char *TAG = "MQTT";
 static const char *subtopic = "/611/AirCondition";
-int delay_flag=0;
 
-#define TIMER_DIVIDER         16  //  Hardware timer clock divider
-#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
 
 #define IRDA_Pin_LOW() gpio_set_level(IRDA_Pin,0)
 #define IRDA_Pin_HIGH() gpio_set_level(IRDA_Pin,1)
 #define Delay_ms(x) vTaskDelay(x/portTICK_PERIOD_MS)
+#define Delay_us(x) ets_delay_us(x)
 void IRDA_AirCondition_Open(void);
 
 
-void IRAM_ATTR timer_group0_isr(void *para)
-{
-    timer_spinlock_take(TIMER_GROUP_0);
-    int timer_idx = (int) para;
 
-    /* Retrieve the interrupt status and the counter value
-       from the timer that reported the interrupt */
-    uint32_t timer_intr = timer_group_get_intr_status_in_isr(TIMER_GROUP_0);
-    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
-
-    /* Clear the interrupt
-       and update the alarm time for the timer with without reload */
-    if (timer_intr & TIMER_INTR_T0) {
-        evt.type = TEST_WITHOUT_RELOAD;
-        timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_0);
-	    delay_flag=1;
-	    
-    	}
-
-    /* After the alarm has been triggered
-      we need enable it again, so it is triggered the next time */
-    timer_spinlock_give(TIMER_GROUP_0);
-}
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {
@@ -141,40 +106,7 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
-static void our_timer_init(int timer_idx,
-                                   bool auto_reload, double timer_interval_sec)
-{
-    /* Select and initialize basic parameters of the timer */
-    timer_config_t config = {
-        .divider = TIMER_DIVIDER,
-        .counter_dir = TIMER_COUNT_UP,
-        .counter_en = TIMER_PAUSE,
-        .alarm_en = TIMER_ALARM_EN,
-        .auto_reload = 0,
-    }; // default clock source is APB
-    timer_init(TIMER_GROUP_0, timer_idx, &config);
 
-    /* Timer's counter will initially start from value below.
-       Also, if auto_reload is set, this value will be automatically reload on alarm */
-    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
-
-    /* Configure the alarm value and the interrupt on alarm. */
-//     timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
-    timer_enable_intr(TIMER_GROUP_0, timer_idx);
-    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr,
-                       (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
-
-//     timer_start(TIMER_GROUP_0, timer_idx);
-}
-
-static void Delay_us(int x)
-{
-	timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
-    	timer_set_alarm_value(TIMER_GROUP_0, timer_idx, x * TIMER_SCALE/1000000);
-	timer_start(TIMER_GROUP_0, timer_idx);
-	while(!delay_flag);
-	delay_flag=0;
-}
 
 void app_main()
 {
@@ -199,15 +131,18 @@ void app_main()
     gpio_set_direction(LED,GPIO_MODE_OUTPUT);
     gpio_pad_select_gpio(IRDA_Pin);
     gpio_set_direction(IRDA_Pin,GPIO_MODE_OUTPUT);
+    gpio_pullup_en(IRDA_Pin);
+    gpio_set_pull_mode(IRDA_Pin,GPIO_PULLUP_ONLY);
+    IRDA_Pin_HIGH();
     mqtt_app_start();
 }
 
 void Start_Code()
 {
 	IRDA_Pin_LOW();
-	Delay_us(9018);
+	Delay_us(9000);
 	IRDA_Pin_HIGH();
-	Delay_us(4435);
+	Delay_us(4433);
 }
 
 void Connect_Code()
@@ -225,10 +160,15 @@ void Finish_Code()
 	IRDA_Pin_HIGH();
 }
 
+void delay_690us()
+{
+    for(int i=0;i<12000;i++);
+}
+
 void Code_1()
 {
 	IRDA_Pin_LOW();
-	Delay_us(691);
+	Delay_us(690);
 	IRDA_Pin_HIGH();
 	Delay_us(1629);
 }
@@ -313,4 +253,5 @@ void IRDA_AirCondition_Open()
 	Code_0();
 	Code_1();
 	Finish_Code();
+
 }
